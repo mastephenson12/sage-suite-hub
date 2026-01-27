c66d08725777d542f0d5fc5d5783becc72e9faab
mastephenson12 <masteph123@gmail.com>
2026-01-27 10:57:37 -0700

    Fix import path for constants in Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { LOGO_DATA_URL, BRAND_NAME, BEEHIIV_URL } from '../constants.ts';
+import { LOGO_DATA_URL, BRAND_NAME, BEEHIIV_URL } from '../constants';
 
 const Navbar: React.FC = () => {
   const [imgError, setImgError] = useState(false);
