49c88149829ce027c0b9cccb60d7c36bc602f6b9
mastephenson12 <masteph123@gmail.com>
2026-01-28 13:40:08 -0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
+// Note: In this specific environment, we don't always need to import CSS 
+// if it's already linked in index.html, but it's good practice.
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
