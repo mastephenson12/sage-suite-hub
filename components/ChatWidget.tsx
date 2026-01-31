35e1c7011c38c9e9d2c524f432a44639fe966961
mastephenson12 <masteph123@gmail.com>
2026-01-30 21:36:54 -0700

    Update ChatWidget.tsx

--- components/ChatWidget.tsx
+++ components/ChatWidget.tsx
@@ -1,6 +1,5 @@
-
 import React, { useState } from 'react';
-import ChatInterface from './ChatInterface.tsx';
+import ChatInterface from './ChatInterface';
 
 const ChatWidget: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
