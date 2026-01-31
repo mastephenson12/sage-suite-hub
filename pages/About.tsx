8ede623fb59267562da70690fb4cba83f5bcb257
mastephenson12 <masteph123@gmail.com>
2026-01-31 10:33:41 -0700

    Update About.tsx

--- pages/About.tsx
+++ pages/About.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BRAND_NAME } from '../constants.ts';
+// Fixed: Removed .ts extension from import
+import { BRAND_NAME } from '../constants';
 
 const About: React.FC = () => {
   return (
@@ -34,10 +35,6 @@ const About: React.FC = () => {
         </div>
       </div>
     </div>
-  );
-};
-
-export default About;
   );
 };
 
