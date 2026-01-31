bbe004c1a97cc22313f4f7568ec99890ec36e239
mastephenson12 <masteph123@gmail.com>
2026-01-31 08:16:36 -0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
 
 console.log("Portal Scout: Hub initialization sequence started.");
 
