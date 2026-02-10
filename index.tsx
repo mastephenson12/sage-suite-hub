8b2c78f921c4479a01d7c523adf72b6c120b0ef6
mastephenson12 <masteph123@gmail.com>
2026-02-10 15:14:33 -0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -6,13 +6,12 @@ const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
   
-  // Render and then signal success
   root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
   );
 
-  // Set the initialization state to 'mounted' to clear the loader in index.html
+  // Signal to index.html that we have successfully mounted
   (window as any).APP_INITIALIZED = 'mounted';
 }
