5b02a8b6f00fa4d7b60e297dd6e94b9492a70118
mastephenson12 <masteph123@gmail.com>
2026-02-12 21:41:58 -0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -10,6 +10,9 @@ if (container) {
       <App />
     </React.StrictMode>
   );
+  
+  // Set a global flag to help index.html clear the loader
+  (window as any).APP_READY = true;
 } else {
   console.error("Critical Failure: Root container not found in DOM.");
 }
