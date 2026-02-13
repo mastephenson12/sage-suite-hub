db8e8dac99675253fef21e360507ad65885c906e
mastephenson12 <masteph123@gmail.com>
2026-02-12 21:19:20 -0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -5,15 +5,11 @@ import App from './App.tsx';
 const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
-  
   root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
   );
-
-  // Set flag immediately
-  (window as any).APP_INITIALIZED = 'mounted';
 } else {
   console.error("Critical Failure: Root container not found in DOM.");
 }
