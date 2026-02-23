3b3ff1b88156fbcc4b33e61f971127d7c0cbc234
mastephenson12 <masteph123@gmail.com>
2026-02-22 20:57:42 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,8 +20,9 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          '@': path.resolve(__dirname, './src'),
         }
       }
     };
 });
+
