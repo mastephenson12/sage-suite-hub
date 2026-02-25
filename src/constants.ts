864a747336ef05bc4f8cbc1df40eefb3c3c79dbd
mastephenson12 <masteph123@gmail.com>
2026-02-25 14:11:05 -0700

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ export const LOGO_DATA_URL = 'https://raw.githubusercontent.com/mastephenson12/s
 export const SAGESUITE_URL = 'https://sage.healthandtravels.com';
 export const SAGESUITE_DIRECTORY = 'https://sage.healthandtravels.com/directory';
 export const SAGESUITE_APPLY = 'https://sage.healthandtravels.com/apply';
-export const BEEHIIV_URL = 'https://healthandtravels.beehiiv.com';
+export const SUBSCRIBE_URL = 'https://healthandtravels.com/subscribe';
 
 export const GHL_CNAME_TARGET = 'flash.funnels.msgsndr.com';
 export const GHL_A_RECORD_IP = '34.16.190.6';
