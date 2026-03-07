52cad78c740f6c8e0b2c2b4914cd6de035fb3cba
mastephenson12 <masteph123@gmail.com>
2026-03-07 10:28:42 -0700

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
-import typography from '@tailwindcss/typography';
+const typography = require('@tailwindcss/typography');
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -31,7 +31,5 @@ export default {
       }
     },
   },
-  plugins: [
-    typography,
-  ],
-}
+  plugins: [typography],
+};
