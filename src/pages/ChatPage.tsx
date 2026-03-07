a955aa21a7f23a49fc70a4659897c6f5a381a63f
mastephenson12 <masteph123@gmail.com>
2026-03-06 18:21:22 -0700

    Update ChatPage.tsx

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ChatInterface from '../components/ChatInterface';
+import { ChatInterface } from '../components/ChatInterface';
 import { ArrowLeft } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
