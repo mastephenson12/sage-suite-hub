47143e6043255286ad776400ca1457661329648e
mastephenson12 <masteph123@gmail.com>
2026-07-25 20:23:54 -0700

    Track packing checklist interactions

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -6,6 +6,7 @@ export type AnalyticsEventName =
   | 'health_travels_ideas_click'
   | 'plan_another_trip_click'
   | 'save_trip_plan_click'
+  | 'packing_checklist_toggle'
   | 'arizona_guides_click'
   | 'share_plan_panel_copy'
   | 'popular_trip_card_click'
